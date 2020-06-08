8fa640336a6a5eb38b9cf3d223cd86f2016d6725
Mehedi Hasan メヘディ ハサン <mehedihasan.cse.jp@gmail.com>
2020-06-08 21:15:15 +0600

    table Calender

--- lib/screens/appoinment_data_picker.dart
+++ lib/screens/appoinment_data_picker.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../components/rounded_button.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class AppoinmentDatePicker extends StatefulWidget {
   static String id = "AppoinmentDatePicker";
@@ -8,6 +8,20 @@ class AppoinmentDatePicker extends StatefulWidget {
 }
 
 class _AppoinmentDatePickerState extends State<AppoinmentDatePicker> {
+  CalendarController _calendarController;
+
+  @override
+  void initState() {
+    super.initState();
+    _calendarController = CalendarController();
+  }
+
+  @override
+  void dispose() {
+    _calendarController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -15,108 +29,120 @@ class _AppoinmentDatePickerState extends State<AppoinmentDatePicker> {
         appBar: AppBar(
           title: Text("Selected  Date"),
         ),
-        body: Container(
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Container(
-                  padding: EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    color: Colors.grey[400],
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Column(
-                    children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Column(
-                            children: <Widget>[
-                              Text("Dr. Mahinur Islam"),
-                              Text("Dr. Mahinur Islam"),
-                              Text("Dr. Mahinur Islam"),
-                              Text("Dr. Mahinur Islam"),
-                            ],
-                          ),
-                          RawMaterialButton(
-                            child: Icon(Icons.person_add),
-                            onPressed: () {},
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(50)),
-                            fillColor: Colors.white,
-                            constraints:
-                                BoxConstraints(minHeight: 60.0, minWidth: 60.0),
-                          ),
-                        ],
-                      ),
-                      Container(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+        body: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[400],
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Column(
+                      children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: <Widget>[
-                            Text("Working Days"),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            Column(
                               children: <Widget>[
-                                Chip(
-                                  label: Text("Sun"),
-                                ),
-                                Chip(
-                                  label: Text("Sun"),
-                                ),
-                                Chip(
-                                  label: Text("Sun"),
-                                ),
-                                Chip(
-                                  label: Text("Sun"),
-                                ),
+                                Text("Dr. Mahinur Islam"),
+                                Text("Dr. Mahinur Islam"),
+                                Text("Dr. Mahinur Islam"),
+                                Text("Dr. Mahinur Islam"),
                               ],
                             ),
-                            Row(
-                              children: <Widget>[
-                                Chip(
-                                  label: Text("Sun"),
-                                ),
-                              ],
+                            RawMaterialButton(
+                              child: Icon(Icons.person_add),
+                              onPressed: () {},
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(50)),
+                              fillColor: Colors.white,
+                              constraints: BoxConstraints(
+                                  minHeight: 60.0, minWidth: 60.0),
                             ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: <Widget>[
-                                Text("Visiting Hours"),
-                                Text("Fee"),
-                              ],
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: <Widget>[
-                                Text("09.00 AM - 12.00 PM"),
-                                Text("500 BDT"),
-                              ],
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: <Widget>[
-                                Text("Reviews"),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: <Widget>[
-                                    Icon(Icons.star),
-                                    Icon(Icons.star),
-                                    Icon(Icons.star),
-                                    Icon(Icons.star),
-                                    Icon(Icons.star)
-                                  ],
-                                )
-                              ],
-                            )
                           ],
                         ),
-                      )
-                    ],
+                        Container(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text("Working Days"),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Chip(
+                                    label: Text("Sun"),
+                                  ),
+                                  Chip(
+                                    label: Text("Sun"),
+                                  ),
+                                  Chip(
+                                    label: Text("Sun"),
+                                  ),
+                                  Chip(
+                                    label: Text("Sun"),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: <Widget>[
+                                  Chip(
+                                    label: Text("Sun"),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Text("Visiting Hours"),
+                                  Text("Fee"),
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Text("09.00 AM - 12.00 PM"),
+                                  Text("500 BDT"),
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Text("Reviews"),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: <Widget>[
+                                      Icon(Icons.star),
+                                      Icon(Icons.star),
+                                      Icon(Icons.star),
+                                      Icon(Icons.star),
+                                      Icon(Icons.star)
+                                    ],
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              )
-            ],
+                Container(
+                  child: TableCalendar(
+                    calendarController: _calendarController,
+                  ),
+                ),
+
+              ],
+            ),
           ),
         ),
       ),
