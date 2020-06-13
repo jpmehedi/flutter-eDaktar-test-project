0e6de01dfbb39762bb278de04ebf8938ced90477
Mehedi Hasan メヘディ ハサン <mehedihasan.cse.jp@gmail.com>
2020-06-13 09:43:18 +0600

    complete patient details  ui

--- lib/screens/new_patint_screen.dart
+++ lib/screens/new_patint_screen.dart
@@ -3,6 +3,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../components/nav_darwer.dart';
 import '../components/inputfield_builder.dart';
 import '../components/dropdown_button_builder.dart';
+import '../components/resueable_long_button.dart';
 
 class NewPatintScreen extends StatefulWidget {
   static String id = 'NewPatintScreen';
@@ -45,127 +46,161 @@ class _NewPatintScreenState extends State<NewPatintScreen> {
             ),
           ],
         ),
-        body: Container(
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'New Patient\'s Details',
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
+        body: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'New Patient\'s Details',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Container(
-                  child: Column(
-                    children: <Widget>[
-                      InputFeildBuilder(
-                        hintText: 'Patient Name',
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Row(
-                        children: <Widget>[
-                          DropdownButtonBuilders(
-                            dropDownButtonTag: 'Age',
-                            dropdownButton: DropdownButton<String>(
-                              items: _age.map((String value) {
-                                return DropdownMenuItem(
-                                  value: value,
-                                  child: Text(value),
-                                );
-                              }).toList(),
-                              onChanged: (value) {},
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Container(
+                    child: Column(
+                      children: <Widget>[
+                        InputFeildBuilder(
+                          hintText: 'Patient Name',
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          children: <Widget>[
+                            DropdownButtonBuilders(
+                              dropDownButtonTag: 'Age',
+                              dropdownButton: DropdownButton<String>(
+                                items: _age.map((String value) {
+                                  return DropdownMenuItem(
+                                    value: value,
+                                    child: Text(value),
+                                  );
+                                }).toList(),
+                                onChanged: (value) {},
+                              ),
                             ),
-                          ),
-                          SizedBox(
-                            width: 20,
-                          ),
-                          DropdownButtonBuilders(
-                            dropDownButtonTag: 'Age',
-                            dropdownButton: DropdownButton<String>(
-                              items: _gender.map((String value) {
-                                return DropdownMenuItem(
-                                  value: value,
-                                  child: Text(value),
-                                );
-                              }).toList(),
-                              onChanged: (value) {},
+                            SizedBox(
+                              width: 20,
                             ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Row(
-                        children: <Widget>[
-                          DropdownButtonBuilders(
-                            dropDownButtonTag: 'Age',
-                            dropdownButton: DropdownButton<String>(
-                              items: _age.map((String value) {
-                                return DropdownMenuItem(
-                                  value: value,
-                                  child: Text(value),
-                                );
-                              }).toList(),
-                              onChanged: (value) {},
+                            DropdownButtonBuilders(
+                              dropDownButtonTag: 'Age',
+                              dropdownButton: DropdownButton<String>(
+                                items: _gender.map((String value) {
+                                  return DropdownMenuItem(
+                                    value: value,
+                                    child: Text(value),
+                                  );
+                                }).toList(),
+                                onChanged: (value) {},
+                              ),
                             ),
-                          ),
-                          SizedBox(
-                            width: 20,
-                          ),
-                          DropdownButtonBuilders(
-                            dropDownButtonTag: 'Age',
-                            dropdownButton: DropdownButton<String>(
-                              items: _gender.map((String value) {
-                                return DropdownMenuItem(
-                                  value: value,
-                                  child: Text(value),
-                                );
-                              }).toList(),
-                              onChanged: (value) {},
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          children: <Widget>[
+                            DropdownButtonBuilders(
+                              dropDownButtonTag: 'Age',
+                              dropdownButton: DropdownButton<String>(
+                                items: _age.map((String value) {
+                                  return DropdownMenuItem(
+                                    value: value,
+                                    child: Text(value),
+                                  );
+                                }).toList(),
+                                onChanged: (value) {},
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                            color: Colors.grey[300],
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 20),
-                          child: TextField(
-                            maxLines: 5,
-                            decoration: InputDecoration(
-                              border:InputBorder.none,
-                              hintText: 'Write Patients Symptoom',
-                              hintStyle: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16
-                              )
-                              
+                            SizedBox(
+                              width: 20,
+                            ),
+                            DropdownButtonBuilders(
+                              dropDownButtonTag: 'Age',
+                              dropdownButton: DropdownButton<String>(
+                                items: _gender.map((String value) {
+                                  return DropdownMenuItem(
+                                    value: value,
+                                    child: Text(value),
+                                  );
+                                }).toList(),
+                                onChanged: (value) {},
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                              color: Colors.grey[300],
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 20),
+                            child: TextField(
+                              maxLines: 5,
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: 'Write Patients Symptoom',
+                                hintStyle: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                ),
+                              ),
                             ),
                           ),
                         ),
-                      )
-                    ],
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          child: Row(
+                            children: <Widget>[
+                              Container(
+                                width: 80,
+                                height: 80,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Colors.grey,
+                                ),
+                                child: Icon(Icons.add,
+                                    size: 50, color: Colors.white),
+                              ),
+                              Container(
+                                width: 120,
+                                height: 140,
+                                child: Image.asset('images/prescription.jpg'),
+                              ),
+                              Container(
+                                width: 120,
+                                height: 140,
+                                child: Image.asset('images/prescription.jpg'),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+                ResueableLongButton(
+                  onPressed: () {},
+                  buttonText: 'Save',
+                ),
+              ],
+            ),
           ),
         ),
       ),
